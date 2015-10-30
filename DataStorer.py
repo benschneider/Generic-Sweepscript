0654eaef17aa2d5c0e1505115cd78667766364bb
Ben <benh.schneider@gmail.com>
2015-10-30 18:52:57 +0100

    Modifications to run simple UP and DOWN sweeps

--- DataStorer.py
+++ DataStorer.py
@@ -1,15 +1,13 @@
 '''
-This file 
+This file
 - Prepare matricies to store data into
 - write result matrix to file using the parser file
-
-
 
 Ben
 2015-10-21
 '''
 import numpy as np
-from time import time, sleep
+# from time import time, sleep
 from parsers import savemtx, make_header, ask_overwrite
 
 
@@ -21,18 +19,30 @@ class DataStoreSP():
     def ask_overwrite(self):
         ask_overwrite(self._folder+self._filen_1)
 
-    def prepare_data_save(self, folder, filen_0, dim_1, dim_2, dim_3, colour_name):
+    def prepare_data_save(self, folder,
+                          filen_0, dim_1, dim_2, dim_3, colour_name):
         self._folder = folder
-        self._filen_1 = filen_0 + '_voltage'  + '.mtx'
+        self._filen_1 = filen_0 + '_voltage' + '.mtx'
         self._head_1 = make_header(dim_1, dim_2, dim_3, colour_name)
         self._matrix3d_1 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
-                
-    def record_data(self,vdata,kk,jj,ii=1):
-        self._matrix3d_1[kk,jj,ii] = vdata
-        
+        if dim_1.UD is True:
+            self.UD = True
+            self._matrix3d_2 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+
+    def record_data(self, vdata, kk, jj, ii=1):
+        self._matrix3d_1[kk, jj, ii] = vdata
+
+    def record_data2(self, vdata, kk, jj, ii=1):
+        self._matrix3d_2[kk, jj, ii] = vdata
+
     def save_data(self):
-        savemtx(self._folder + self._filen_1, self._matrix3d_1, header = self._head_1)
-                
+        savemtx(self._folder + self._filen_1,
+                self._matrix3d_1,
+                header=self._head_1)
+        if self.UD is True:
+            savemtx(self._folder + self._filen_1 + '_2',
+                    self._matrix3d_2,
+                    header=self._head_1)
 
 
 class DataStoreVNA():
@@ -42,12 +52,12 @@ class DataStoreVNA():
 
     def ask_overwrite(self):
         ask_overwrite(self._folder+self._filen_1)
-        
+
     def prepare_data_save(self, folder, filen_0, dim_1, dim_2, dim_3):
         self._folder = folder
-        self._filen_1 = filen_0 + '_real'  + '.mtx'
-        self._filen_2 = filen_0 + '_imag'  + '.mtx'
-        self._filen_3 = filen_0 + '_mag'   + '.mtx'
+        self._filen_1 = filen_0 + '_real' + '.mtx'
+        self._filen_2 = filen_0 + '_imag' + '.mtx'
+        self._filen_3 = filen_0 + '_mag' + '.mtx'
         self._filen_4 = filen_0 + '_phase' + '.mtx'
         self._head_1 = make_header(dim_1, dim_2, dim_3, 'S21 _real')
         self._head_2 = make_header(dim_1, dim_2, dim_3, 'S21 _imag')
@@ -57,23 +67,30 @@ class DataStoreVNA():
         self._matrix3d_2 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
         self._matrix3d_3 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
         self._matrix3d_4 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
-                
-    def record_data(self,vnadata,kk,jj,ii=1):
-        self._phase_data = np.angle(vnadata)        
+
+    def record_data(self, vnadata, kk, jj, ii=1):
+        self._phase_data = np.angle(vnadata)
         if vnadata.shape[0] == 1:
-            self._matrix3d_1[kk,jj,ii] = vnadata.real
-            self._matrix3d_2[kk,jj,ii] = vnadata.imag
-            self._matrix3d_3[kk,jj,ii] = np.absolute(vnadata)
-            self._matrix3d_4[kk,jj,ii] = self._phase_data
+            self._matrix3d_1[kk, jj, ii] = vnadata.real
+            self._matrix3d_2[kk, jj, ii] = vnadata.imag
+            self._matrix3d_3[kk, jj, ii] = np.absolute(vnadata)
+            self._matrix3d_4[kk, jj, ii] = self._phase_data
         else:
-            self._matrix3d_1[kk,jj] = vnadata.real
-            self._matrix3d_2[kk,jj] = vnadata.imag
-            self._matrix3d_3[kk,jj] = np.absolute(vnadata)
-            self._matrix3d_4[kk,jj] = np.unwrap(self._phase_data)
-        
+            self._matrix3d_1[kk, jj] = vnadata.real
+            self._matrix3d_2[kk, jj] = vnadata.imag
+            self._matrix3d_3[kk, jj] = np.absolute(vnadata)
+            self._matrix3d_4[kk, jj] = np.unwrap(self._phase_data)
+
     def save_data(self):
-        savemtx(self._folder + self._filen_1, self._matrix3d_1, header = self._head_1)
-        savemtx(self._folder + self._filen_2, self._matrix3d_2, header = self._head_2)
-        savemtx(self._folder + self._filen_3, self._matrix3d_3, header = self._head_3)
-        savemtx(self._folder + self._filen_4, self._matrix3d_4, header = self._head_4)
-                
+        savemtx(self._folder + self._filen_1,
+                self._matrix3d_1,
+                header=self._head_1)
+        savemtx(self._folder + self._filen_2,
+                self._matrix3d_2,
+                header=self._head_2)
+        savemtx(self._folder + self._filen_3,
+                self._matrix3d_3,
+                header=self._head_3)
+        savemtx(self._folder + self._filen_4,
+                self._matrix3d_4,
+                header=self._head_4)
--- sweepscript_UD.py
+++ sweepscript_UD.py
@@ -24,17 +24,17 @@ vm = key2000('GPIB0::29::INSTR')
 iBias = yoko('GPIB0::13::INSTR',
            name = 'Yoko V R=(998.83+14.24)KOhm',
            start = -6,
-           stop = 6, 
+           stop = 6,
            pt = 601,
            sstep = 0.1, # def max voltage steps it can take
-           stime = 0.1) 
+           stime = 0.1)
 iBias.prepare_v(vrange = 5)  # vrange =2 -- 10mV, 3 -- 100mV, 4 -- 1V, 5 -- 10V, 6 -- 30V
 iBias.UD = True
 
 vMag = yoko('GPIB0::10::INSTR',
             name = 'Magnet V R=2.19KOhm',
             start = -280e-3,
-            stop = 360e-3, 
+            stop = 360e-3,
             pt = 321,
             sstep = 5e-3,
             stime = 1e-6)
@@ -46,9 +46,9 @@ PSG = dummy('GPIB0::11::INSTR',
            stop = 0,
            pt = 1,
            sstep = 20e-3,
-           stime = 1e-3) 
+           stime = 1e-3)
 
-iBias.sweep_par = 'v' 
+iBias.sweep_par = 'v'
 vMag.sweep_par = 'v'
 
 dim_1= iBias
@@ -72,25 +72,29 @@ print 'Executing sweep'
 print 'req time (min):'+str(dim_3.pt*dim_2.pt*dim_1.pt*0.032/60)
 t0 = time()
 try:
-    for kk in range(dim_3.pt): 
+    for kk in range(dim_3.pt):
         sweep_dim_3(dim_3,dim_3.lin[kk])
         sweep_dim_2(dim_2,dim_2.start)
         for jj in range(dim_2.pt):
-            sweep_dim_2(dim_2,dim_2.lin[jj])            
-            
+            sweep_dim_2(dim_2,dim_2.lin[jj])
             sweep_dim_1(dim_1,dim_1.start)
-            if dim_1.UD == True:
-                pass
-                
+            if dim_1.UD is True:
+                for ii in range(dim_1.pt):
+                    dim_1.set_v2(dim_1.lin[ii])
+                    vdata = vm.get_val()
+                    DS.record_data(vdata,kk,jj,ii)
+
+                for ii in range((dim_1.pt-1),-1,-1):
+                    dim_1.set_v2(dim_1.lin[ii])
+                    vdata = vm.get_val()
+                    DS.record_data2(vdata,kk,jj,ii)
             else:
-                pass
-            
-            for ii in range(dim_1.pt):
-                dim_1.set_v2(dim_1.lin[ii])
-                vdata = vm.get_val()
-                DS.record_data(vdata,kk,jj,ii)
-                       
-            DS.save_data()            
+                for ii in range(dim_1.pt):
+                    dim_1.set_v2(dim_1.lin[ii])
+                    vdata = vm.get_val()
+                    DS.record_data(vdata,kk,jj,ii)
+
+            DS.save_data()
             t1 = time()
             remaining_time = ((t1-t0)/(jj+1)*dim_2.pt*dim_3.pt - (t1-t0))
             print 'req time (h):'+str(remaining_time/3600)
@@ -98,7 +102,7 @@ try:
 
 except (KeyboardInterrupt):
     print '***** Keyboart Interupt *****'
-    
+
 finally:
     print 'Time used min:' +str((time()-t0)/60)
     print 'Yokos -> zero and switch off'
