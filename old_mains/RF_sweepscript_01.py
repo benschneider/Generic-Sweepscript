07f08aedef63be5db674406d4a98198d041016c7
Ben <benh.schneider@gmail.com>
2015-11-06 16:14:08 +0100

    even older code to keep track of

---
+++ old_mains/RF_sweepscript_01.py
@@ -0,0 +1,161 @@
+'''
+Generic Sweep script
+(currently setup for no more than 3 dims)
+
+22/06/2015
+- B
+'''
+
+#how to update functions of a module when loading from console ? ??? or tell Python to recompile its files :
+import ramp_mod, parsers, RSZNB20, Yoko, testdriver
+reload(RSZNB20)
+reload(Yoko)
+reload(parsers)
+reload(ramp_mod)
+reload(testdriver)
+del ramp_mod, parsers, RSZNB20, Yoko, testdriver
+
+import numpy as np
+from time import time, sleep
+from inspect import currentframe, getfile #
+thisfile = getfile(currentframe())
+from parsers import savemtx, ask_overwrite, copy_file, make_header
+from ramp_mod import ramp
+import sys
+
+filen_0 = 'S1_145'
+folder = 'data\\'
+
+from Yoko import instrument as i2
+dim_1b = i2('GPIB0::10::INSTR') #'Yoko M' 
+dim_1b.name = 'Yoko M'
+dim_1b.start = -0.5 
+dim_1b.stop = 0.5
+
+from RSZNB20 import instrument as i1
+dim_1 = i1('TCPIP::169.254.107.192::INSTR')
+dim_1.name = dim_1b.name #'ZNB20 S21 - Time'
+dim_1.start = dim_1b.start
+dim_1.stop = dim_1b.stop
+vdata = dim_1.get_data()
+dim_1.pt = vdata.shape[0]    
+dim_1.sweeptime = dim_1.get_sweeptime()
+if dim_1.sweeptime < 3:
+    sys.exit("Dim_1 Sweeptime is too short")
+
+dim_1b.time = dim_1.sweeptime #second sweep while VNA records
+dim_1b.set_mode(1) #V mode
+dim_1b.set_vrange(4) #2  10mV, 3  100mV, 4  1V, 5  10V, 6  30V
+dim_1b.sweep_v(0.0,4) #sweep to 0 V
+sleep(4.1)
+dim_1b.output(1) #turn Yoko ON
+
+def sweep_dim1(dim_1,dim_1b):
+    '''
+    1. sweep magnet to start position
+    2. sweep magnet at the same time with the VNA
+    3. return VNA data
+    '''
+    dim_1b.sweep_v(dim_1b.start, 3)
+    sleep(3.2)
+    dim_1.init_sweep()
+    dim_1b.sweep_v(dim_1b.stop, dim_1b.time)        
+    sleep(dim_1b.time+1)
+    vdata = dim_1.get_data() # np.array(real+ i* imag)
+    if vdata == 'Error':
+        print 'retaking this trace'
+        vdata = sweep_dim1(dim_1,dim_1b) #infinite subloops if an error persists :D
+    return vdata
+
+from Yoko import instrument as i3
+dim_2 = i3('GPIB0::13::INSTR') #'Yoko V' 
+dim_2.name = 'Yoko V 14.8KOhm' 
+dim_2.start = -2e-3
+dim_2.stop = 4e-3
+dim_2.pt = 61
+
+dim_2.set_mode(1)
+dim_2.set_vrange(3) 
+dim_2.sweep_v(0.0,4)
+sleep(4.1)
+dim_2.output(1)
+sleep(1)
+
+from testdriver import instrument as i4 #just a dummy driver
+dim_3 = i4('Nothing') #VNA POWER sweep
+dim_3.name = 'RF-CW-FREQ' 
+dim_3.start = 6e9
+dim_3.stop = 9e9
+dim_3.pt = 31
+dim_3.set_freq_cw = dim_1.set_freq_cw
+
+'''
+#Other Equipment
+execfile('keithley2000.py')
+vm = instrument3('GPIB0::29::INSTR')
+vm.optimise()
+sleep(0.1)
+vm.testspeed()
+'''
+
+#for the VNA I want 4 data files for Real, Imag, MAG, Phase
+filen_1 = filen_0 + '_real'  + '.mtx'
+filen_2 = filen_0 + '_imag'  + '.mtx'
+filen_3 = filen_0 + '_mag'   + '.mtx'
+filen_4 = filen_0 + '_phase' + '.mtx'
+head_1 = make_header(dim_1, dim_2, dim_3, 'S21 _real')
+head_2 = make_header(dim_1, dim_2, dim_3, 'S21 _imag')
+head_3 = make_header(dim_1, dim_2, dim_3, 'S21 _mag')
+head_4 = make_header(dim_1, dim_2, dim_3, 'S21 _phase')
+matrix3d_1 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+matrix3d_2 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+matrix3d_3 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+matrix3d_4 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+
+ask_overwrite(folder+filen_1)
+copy_file(thisfile, filen_0, folder) #backup this script
+
+dim_1.lin = np.linspace(dim_1.start,dim_1.stop,dim_1.pt)
+dim_2.lin = np.linspace(dim_2.start,dim_2.stop,dim_2.pt)
+dim_3.lin = np.linspace(dim_3.start,dim_3.stop,dim_3.pt)
+
+print 'Executing sweep'
+print 'req time (h):'+str(dim_3.pt*dim_2.pt*dim_1.sweeptime/3600)
+t0 = time()
+try:
+    for kk in range(dim_3.pt): 
+        dim_3val = dim_3.lin[kk] 
+        dim_3.set_freq_cw(dim_3val)
+        #sleep(5.2)
+
+        dim_2.sweep_v(dim_2.start, 4)
+        sleep(4.2)
+        for jj in range(dim_2.pt):
+            dim_2val = dim_2.lin[jj] 
+            dim_2.sweep_v(dim_2val, 0.1)
+            
+            vdata = sweep_dim1(dim_1,dim_1b) #sweep dim1 & dim_1b
+            phase_data = np.angle(vdata)        
+            matrix3d_1[kk,jj] = vdata.real
+            matrix3d_2[kk,jj] = vdata.imag
+            matrix3d_3[kk,jj] = np.absolute(vdata)
+            matrix3d_4[kk,jj] = np.unwrap(phase_data)
+            savemtx(folder + filen_1, matrix3d_1, header = head_1)
+            savemtx(folder + filen_2, matrix3d_2, header = head_2)
+            savemtx(folder + filen_3, matrix3d_3, header = head_3)
+            savemtx(folder + filen_4, matrix3d_4, header = head_4)
+            
+            t1 = time()
+            remaining_time = ((t1-t0)/(jj+1)*dim_2.pt*dim_3.pt - (t1-t0))
+            print 'req time (h):'+str(remaining_time/3600)
+
+finally:
+    print 'Finish measurements'
+    print 'Time used :' +str(time()-t0)
+    print 'Yokos -> zero and switch off'
+    dim_1b.sweep_v(0, 5)
+    dim_2.sweep_v(0, 5)
+    sleep(5.2)
+    dim_1b.output(0) #Yoko Off
+    dim_2.output(0)
+    
---
+++ old_mains/mainstart.py
@@ -0,0 +1,92 @@
+'''
+Generic Sweep script
+
+
+22/06/2015
+- B
+'''
+
+#import visa
+import numpy as np
+from time import sleep
+from time import time
+
+execfile('parsers.py')
+
+#driver
+execfile('RSZNB20.py')
+vna = instrument1('TCPIP::169.254.107.192::INSTR')
+execfile('Yoko.py')
+yokoM = instrument2('GPIB0::10::INSTR')
+yokoV = instrument2('GPIB0::13::INSTR')
+execfile('keithley2000.py')
+vm = instrument3('GPIB0::29::INSTR')
+
+
+#setup 
+vm.optimise()
+sleep(0.1)
+vm.testspeed()
+
+filen_1 = 'ben_data_130.mtx'
+
+start_v = -0.1
+stop_v = 0.15
+
+v_swt = 297 #set time for voltage sweep
+
+averages = 100
+
+'''
+vdata = get_vnadata()  #grab data from vna to know data sizes...
+
+
+#Prepare results matrix
+matrix3d = np.zeros(( 4, averages, vdata.shape[0]))
+
+t0 = time()
+
+#execute sweep
+for jj in range(averages):
+    #voltage - start
+    sweep_v(start_v,5)
+    sleep(6)
+
+    #start VNA sweep
+
+
+    #voltage - end
+    sweep_v(stop_v,v_swt)
+    sleep(v_swt+2)
+
+    #grab [real, imag]  from VNA
+    vdata = get_vnadata()
+
+    #save into the matrix
+    matrix3d[0,jj] = vdata.real
+    matrix3d[1,jj] = vdata.imag
+    matrix3d[2,jj] = np.absolute(vdata)
+    phase_data = np.angle(vdata)
+    matrix3d[3,jj] = np.unwrap(phase_data)
+
+
+    #store data into a file
+    head1 = ['Units', 'real-imag-mag-phase',
+            'Yoko', str(start_v), str(stop_v),
+            'Y-label', str(jj), '0',
+            'Z-label', '1', '4']
+    savemtx(filen_1, matrix3d, header = head1)
+
+    t1 = time()
+    remaining_time = ((t1-t0)/(jj+1)*averages - (t1-t0))
+    print remaining_time #seconds
+
+#Finish measurements
+#return Yoko to zero and switch off
+sweep_v(0,5)
+sleep(6)
+#yoko.write('O0 E')
+#Leftovers to be deleted later
+#def set_voltage(value):
+#    yoko.write('S'+str(value)+' E')
+'''
