5d3c4b510b947a2701f7caf6a3252a15dba99179
Ben <benh.schneider@gmail.com>
2015-11-08 02:31:43 +0100

    Untested Prototype driver

--- Digitizer.py
+++ Digitizer.py
@@ -11,25 +11,69 @@ class instrument():
     '''Upon start, creates connection to Digitizer and Local oscillator
         this driver only contains some rudimentary functions to run
         the PXI-Aeroflex 3036 Digitizer.
+
+        To speed things up one function is used to record data which is
+        then stored in the object.
+        From this various aspects are then calculated as needed and
+        retriggered/recoreded again when actually needed instead of every time
+        again...
+
     '''
 
-    def __init__(self, aDigi='3036D1', aLo='3011D1', name='D',
+    def __init__(self, adressDigi='3036D1', adressLo='3011D1', name='D',
                  start=0, stop=0, pt=1, sstep=20e-3, stime=1e-3):
-        self.adressDigi = aDigi
+        self.adressDigi = adressDigi
         self.Overload = 3  # to test the overload code
-        self.adressLo = aLo
+        self.adressLo = adressLo
         self.name = name
         self.start = start
         self.stop = stop
         self.pt = pt
         self.lin = np.linspace(self.start, self.stop, self.pt)
         self.digitizer = PXIDigitizer_wrapper.afDigitizer_BS()
-        self.digitizer.create_object()
-        self.digitizer.boot_instrument(aLo, aDigi)
-        self.prep_data()
+        try:
+            self.digitizer.create_object()
+            self.digitizer.boot_instrument(adressLo, adressDigi)
+            self.prep_data()
+            self.set_settings()
+        except PXIDigitizer_wrapper.Error as e:
+            self.raiseOnError(e)
+
+    def raiseOnError(self, e):
+            print str(e), sys.exc_info()[0]
+            raise
+
+    def performClose(self, bError=False):
+        ''' assume digitizer exists'''
+        try:
+            self.digitizer.rf_rf_input_level_set(30)
+            self.digitizer.close_instrument(bCheckError=not bError)
+        except PXIDigitizer_wrapper.Error as e:
+            if not bError:
+                self.raiseOnError(e)
+        finally:
+            try:
+                self.digitizer.destroy_object()
+                del self.digitizer
+            except:
+                pass
 
     def set_settings(self):
-        pass
+        '''right now its easies to simply set the settings here'''
+        self.sampFreq = 10e6        # in Hz
+        self.bandwidth = 10e6
+        self.avgPerTrig = 1
+        self.removeDCoff = 1
+        self.LoPos = 1              # Lo Above (1) or Below (0)
+        self.freq = 5e9
+        self.nSamples = 50e3        # Samples taken/trigger
+        self.digitizer.rf_remove_dc_offset_set(bool(self.removeDCoff))
+        self.digitizer.modulation_generic_sampling_frequency_set(self.sampFreq)
+        # self.digitizer.trigger_IQ_bandwidth_set(self.bandwidth, iOption)
+        self.digitizer.rf_rf_input_level_set(-10)
+        self.digitizer.rf_userLOPosition_set(self.LoPos)
+        self.digitizer.modulation_mode_set(5)  # Set to Generic
+        self.digitizer.rf_centre_frequency_set(self.freq)
 
     def prep_data(self):
         self.cAvgSignal = None
@@ -43,19 +87,109 @@ class instrument():
         self.cRaw = None
         self.dPower = None
 
-    def set_freq(self, var_1):
-        sleep(0.01)
-        self.var_1 = var_1
-        print var_1
+    def set_freq(self, freq):
+        self.digitizer.rf_centre_frequency_set(freq)
 
     def set_inputLevel(self, value):
-        pass
-
-    def get_IQ(self):
-        return self.var_1
+        self.digitizer.rf_rf_input_level_set(value)
 
     def get_Levelcorr(self):
-        return (np.random.rand(1)*10-5)*2
+        return self.digitizer.rf_level_correction_get()
+
+    def set_LoAboveBelow(self, val):
+        ''' 0 is below 1 is above'''
+        self.digitizer.rf_userLOPosition_set(val)
+
+    def getIQTrace(self):
+        """Return I and Q signal in time as a complex vector,
+        resample the signal if needed, and then clear cTrace to indicate
+        capture has been taken."""
+        if self.cTrace is None:
+            self.sampleAndAverage()
+        vTrace = self.cTrace
+        self.cTrace = None
+        return vTrace
+
+    def sampleAndAverage(self):
+        '''Sample the signal, calc I+j*Q theta
+        and store it in the driver object
+        of each triger there are x samples... which can be averaged...
+        '''
+        TriggerSourceValue = self.digitizer.trigger_source_get()
+        dLevelCorrection = self.digitizer.rf_level_correction_get()
+        self.digitizer.trigger_arm_set(self.nSamples*2)
+        self.checkADCOverload()
+
+        vI = np.zeros(self.nSamples)
+        vQ = np.zeros(self.nSamples)
+        vI2 = np.zeros(self.nSamples)
+        vQ2 = np.zeros(self.nSamples)
+        vPowerMean = np.zeros(1)
+        vIMean = np.zeros(1)
+        vQMean = np.zeros(1)
+        MMag = np.zeros(1)
+        MPhase = np.zeros(1)
+
+
+        # Trigger source 32 = SW trigger
+        if TriggerSourceValue is not 32:
+            # Arm the trigger with 2*inSamples
+            self.digitizer.trigger_arm_set(self.nSamples*2)
+            self.checkADCOverload()
+            while self.digitizer.data_capture_complete_get() is False:
+                self.thread().msleep(1)
+                self.checkADCOverload()
+                (lI, lQ) = self.digitizer.capture_iq_capt_mem(self.nSamples)
+        else:
+                self.checkADCOverload()
+                (lI, lQ) = self.digitizer.capture_iq_capt_mem(self.nSamples)
+
+        # The raw data is stored as a long array,
+        # continously appending the newly aquired data
+        if self.bRaw:
+            vectorI = (np.array(lI)
+                       * np.power(10.0, dLevelCorrection/20.0)
+                       / np.sqrt(1000))
+            vectorQ = (np.array(lQ)
+                       * np.power(10.0, dLevelCorrection/20.0)
+                       / np.sqrt(1000))
+
+        # scale data to Volt / sqrt(1Ohm)
+        scaledI = (np.array(lI)
+                   * np.power(10.0, dLevelCorrection/20.0)
+                   / np.sqrt(1000))
+        scaledQ = (np.array(lQ)
+                   * np.power(10.0, dLevelCorrection/20.0)
+                   / np.sqrt(1000))
+        crep = scaledI + 1j*scaledQ
+
+        # We add the aquired data to the vI and vQ arrays
+        vI = vI + np.mean(scaledI, axis=0)
+        vQ = vQ + np.mean(scaledQ, axis=0)
+        vI2 = vI2 + np.mean(scaledI**2, axis=0)
+        vQ2 = vQ2 + np.mean(scaledQ**2, axis=0)
+
+
+        # Result data being stored
+        self.vPowerMean = (np.mean(scaledI**2, axis=1)
+                           + np.mean(scaledQ**2, axis=1))
+        self.vIMean = np.mean(scaledI, axis=1)
+        self.vQMean = np.mean(scaledQ, axis=1)
+        self.MeanMag = np.mean(np.absolute(crep), axis=1)
+        self.MeanPhase = np.mean(np.angle(crep), axis=1)
+
+        self.vMeanUnAvg = vIMean+1j*vQMean
+        self.cTrace = vI+1j*vQ
+        self.vPTrace = (vI2+vQ2)
+        self.cAvgSignal = np.average(vI)+1j*np.average(vQ)
+        self.cAvgSignal2 = np.rect(self.MeanMag, self.MeanPhas)
+        self.dPower = np.average(vI2)+np.average(vQ2)
+
+        # Return the non averaged vectors if wanted
+        if self.bRaw:
+            self.cRaw = vectorI + 1j*vectorQ
+            vectorI = None
+            vectorQ = None
 
     def get_Vcorr(self):
         pass
@@ -66,11 +200,9 @@ class instrument():
     def get_bandwidth(self):
         return 0
 
-    def set_sampRater(self, val):
-        self.digitizer.modulation_generic_sampling_frequency_set(value)
+    def set_sampRate(self, val):
+        self.digitizer.modulation_generic_sampling_frequency_set(val)
 
-    # Check if the ADC overloaded and if it was put the max input level
-    # to +30dBm and raise an error
     def checkADCOverload(self):
         if self.digitizer.check_ADCOverload():
             self.Overload = self.Overload + 1
@@ -79,7 +211,5 @@ class instrument():
                 self.digitizer.rf_rf_input_level_set(30)
                 print 'ADC overloaded 3x in a row', sys.exc_info()[0]
                 raise
-                # raise InstrumentDriver.CommunicationError('ADC overloaded hence the measurement is stopped and the max input level on the digitizer is put to +30 dBm')
         else:
             self.Overload = 0
-
