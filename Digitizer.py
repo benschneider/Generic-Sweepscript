9d76ffcd41798ff8ef9b5aee238eeaf61712b9a8
Ben <benh.schneider@gmail.com>
2015-11-08 12:58:55 +0100

    removed usless and moved usefull

--- Digitizer.py
+++ Digitizer.py
@@ -1,4 +1,4 @@
-from time import sleep  # , time
+from time import sleep
 import numpy as np
 import sys
 import PXIDigitizer_wrapper
@@ -8,23 +8,31 @@ import PXIDigitizer_wrapper
 
 
 class instrument():
+
     '''Upon start, creates connection to Digitizer and Local oscillator
         this driver only contains some rudimentary functions to run
         the PXI-Aeroflex 3036 Digitizer.
-
         To speed things up one function is used to record data which is
         then stored in the object.
         From this various aspects are then calculated as needed and
         retriggered/recoreded again when actually needed instead of every time
         again...
-
     '''
 
     def __init__(self, adressDigi='3036D1', adressLo='3011D1', name='D',
                  start=0, stop=0, pt=1, sstep=20e-3, stime=1e-3):
-        self.adressDigi = adressDigi
-        self.Overload = 3  # to test the overload code
+        self.sampFreq = 10e6        # in Hz
+        self.bandwidth = 10e6
+        self.avgPerTrig = 1
+        self.removeDCoff = 1
+        self.LoPos = 1              # Lo Above (1) or Below (0)
+        self.freq = 5e9
+        self.nSamples = 50e3        # Samples taken/trigger
+        self.Overload = 3           # to test the overload code
+        self.LoRef = 0              # 0=ocxo, 1=int 2=extDaisy, 3=extTerminated
+        self.trig_source = 8        # 8=Star, 32=SW, 35=internal
         self.adressLo = adressLo
+        self.adressDigi = adressDigi
         self.name = name
         self.start = start
         self.stop = stop
@@ -36,6 +44,7 @@ class instrument():
             self.digitizer.boot_instrument(adressLo, adressDigi)
             self.prep_data()
             self.set_settings()
+            print self.digitizer.ref_is_locked()  # print Lo locked confirmation
         except PXIDigitizer_wrapper.Error as e:
             self.raiseOnError(e)
 
@@ -59,14 +68,9 @@ class instrument():
                 pass
 
     def set_settings(self):
-        '''right now its easies to simply set the settings here'''
-        self.sampFreq = 10e6        # in Hz
-        self.bandwidth = 10e6
-        self.avgPerTrig = 1
-        self.removeDCoff = 1
-        self.LoPos = 1              # Lo Above (1) or Below (0)
-        self.freq = 5e9
-        self.nSamples = 50e3        # Samples taken/trigger
+        '''
+        #right now its easies to simply set the settings here
+        '''
         self.digitizer.rf_remove_dc_offset_set(bool(self.removeDCoff))
         self.digitizer.modulation_generic_sampling_frequency_set(self.sampFreq)
         # self.digitizer.trigger_IQ_bandwidth_set(self.bandwidth, iOption)
@@ -74,8 +78,12 @@ class instrument():
         self.digitizer.rf_userLOPosition_set(self.LoPos)
         self.digitizer.modulation_mode_set(5)  # Set to Generic
         self.digitizer.rf_centre_frequency_set(self.freq)
+        self.digitizer.lo_reference_set(self.LoRef)
+        self.digitizer.trigger_source_set(self.trig_source)
 
     def prep_data(self):
+        self.vectorI = None
+        self.vectorQ = None
         self.cAvgSignal = None
         self.cAvgSignal2 = None
         self.cTrace = None
@@ -97,7 +105,9 @@ class instrument():
         return self.digitizer.rf_level_correction_get()
 
     def set_LoAboveBelow(self, val):
-        ''' 0 is below 1 is above'''
+        '''
+        #0 is below 1 is above
+        '''
         self.digitizer.rf_userLOPosition_set(val)
 
     def getIQTrace(self):
@@ -111,31 +121,26 @@ class instrument():
         return vTrace
 
     def sampleAndAverage(self):
-        '''Sample the signal, calc I+j*Q theta
-        and store it in the driver object
-        of each triger there are x samples... which can be averaged...
+        '''# Sample the signal, calc I+j*Q theta
+        # and store it in the driver object
+        # of each triger there are x samples... which can be averaged...
         '''
         TriggerSourceValue = self.digitizer.trigger_source_get()
         dLevelCorrection = self.digitizer.rf_level_correction_get()
         self.digitizer.trigger_arm_set(self.nSamples*2)
         self.checkADCOverload()
-
         vI = np.zeros(self.nSamples)
         vQ = np.zeros(self.nSamples)
         vI2 = np.zeros(self.nSamples)
         vQ2 = np.zeros(self.nSamples)
-        vPowerMean = np.zeros(1)
         vIMean = np.zeros(1)
         vQMean = np.zeros(1)
-        MMag = np.zeros(1)
-        MPhase = np.zeros(1)
 
-
-        # Trigger source 32 = SW trigger
+        # TriggersourceValue 32 = SW trigger
         if TriggerSourceValue is not 32:
-            # Arm the trigger with 2*inSamples
             self.digitizer.trigger_arm_set(self.nSamples*2)
             self.checkADCOverload()
+            # wait for external trigger signal
             while self.digitizer.data_capture_complete_get() is False:
                 self.thread().msleep(1)
                 self.checkADCOverload()
@@ -143,16 +148,6 @@ class instrument():
         else:
                 self.checkADCOverload()
                 (lI, lQ) = self.digitizer.capture_iq_capt_mem(self.nSamples)
-
-        # The raw data is stored as a long array,
-        # continously appending the newly aquired data
-        if self.bRaw:
-            vectorI = (np.array(lI)
-                       * np.power(10.0, dLevelCorrection/20.0)
-                       / np.sqrt(1000))
-            vectorQ = (np.array(lQ)
-                       * np.power(10.0, dLevelCorrection/20.0)
-                       / np.sqrt(1000))
 
         # scale data to Volt / sqrt(1Ohm)
         scaledI = (np.array(lI)
@@ -169,7 +164,6 @@ class instrument():
         vI2 = vI2 + np.mean(scaledI**2, axis=0)
         vQ2 = vQ2 + np.mean(scaledQ**2, axis=0)
 
-
         # Result data being stored
         self.vPowerMean = (np.mean(scaledI**2, axis=1)
                            + np.mean(scaledQ**2, axis=1))
@@ -184,12 +178,6 @@ class instrument():
         self.cAvgSignal = np.average(vI)+1j*np.average(vQ)
         self.cAvgSignal2 = np.rect(self.MeanMag, self.MeanPhas)
         self.dPower = np.average(vI2)+np.average(vQ2)
-
-        # Return the non averaged vectors if wanted
-        if self.bRaw:
-            self.cRaw = vectorI + 1j*vectorQ
-            vectorI = None
-            vectorQ = None
 
     def get_Vcorr(self):
         pass
