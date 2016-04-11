449795fc9d551f0f8ac2573a312c400975c5c2f3
Ben <benh.schneider@gmail.com>
2016-04-11 18:55:59 +0200

    Comdriver

--- old_driver/PXI_3036Digi.py
+++ old_driver/PXI_3036Digi.py
@@ -4,10 +4,10 @@ Created on Thu Apr 07 15:06:04 2016
 
 @author: Morran or Lumi
 """
-#from comtypes.client import CreateObject
+from comtypes.client import CreateObject
 #D1=CreateObject('afComDigitizer.afCoDigitizer')
 # C:\Python27\Lib\site-packages\comtypes\gen
-#from comtypes.gen import AFCOMDIGITIZERLib as afdiglib
+from comtypes.gen import AFCOMDIGITIZERLib as afdiglib
 #print  afdiglib.afDigitizerDll_tsSW_TRIG
 #D1.BootInstrument('3011D1', '3036D1', False)
 #print  D1.ErrorCode,  D1.ErrorSource, D1.ErrorMessage
@@ -16,7 +16,7 @@ Created on Thu Apr 07 15:06:04 2016
 #print D1.RF.CentreFrequency
 #D1.CloseInstrument()
 
-from ctypes import c_int, c_long, c_float, c_double, c_ulong, POINTER, byref, WinDLL, c_char_p, Structure, c_void_p
+# from ctypes import c_int, c_long, c_float, c_double, c_ulong, POINTER, byref, WinDLL, c_char_p, Structure, c_void_p
 
 class digitizer(object):
     
@@ -33,8 +33,8 @@ class digitizer(object):
 
     @error_check
     def __init__(self):
-        #self.D=CreateObject('afComDigitizer.afCoDigitizer')  # the com driver does not have Buffer options available
-        self.D = WinDLL('afDigitizerDll_32')
+        self.D=CreateObject('afComDigitizer.afCoDigitizer')  # the com driver does not have Buffer options available
+        
         
     def startup(self):
         pass
@@ -73,15 +73,15 @@ class digitizer(object):
         pass
 
     
-    def capture_iq_issue_buffer(self, buffer_ref, capture_ref, timeout=1):
-        obj=getDllObject('afDigitizerDll_Capture_IQ_IssueBuffer',
-                          argtypes = [afDigitizerInstance_t, POINTER(afDigitizerBufferIQ_t), c_long, POINTER(afDigitizerCaptureIQ_t)])
-        error=obj(self.session, byref(buffer_ref), 1000*timeout, byref(capture_ref))
-        self.check_error(error)
-
-    def capture_iq_reclaim_buffer(self, capture_ref, buffer_ref_pointer):
-        obj=getDllObject('afDigitizerDll_Capture_IQ_ReclaimBuffer',
-                          argtypes = [afDigitizerInstance_t, afDigitizerCaptureIQ_t, POINTER(POINTER(afDigitizerBufferIQ_t))])
-        error = obj(self.session, capture_ref, byref(buffer_ref_pointer))
-        self.check_error(error)
-           
+#    def capture_iq_issue_buffer(self, buffer_ref, capture_ref, timeout=1):
+#        obj=getDllObject('afDigitizerDll_Capture_IQ_IssueBuffer',
+#                          argtypes = [afDigitizerInstance_t, POINTER(afDigitizerBufferIQ_t), c_long, POINTER(afDigitizerCaptureIQ_t)])
+#        error=obj(self.session, byref(buffer_ref), 1000*timeout, byref(capture_ref))
+#        self.check_error(error)
+#
+#    def capture_iq_reclaim_buffer(self, capture_ref, buffer_ref_pointer):
+#        obj=getDllObject('afDigitizerDll_Capture_IQ_ReclaimBuffer',
+#                          argtypes = [afDigitizerInstance_t, afDigitizerCaptureIQ_t, POINTER(POINTER(afDigitizerBufferIQ_t))])
+#        error = obj(self.session, capture_ref, byref(buffer_ref_pointer))
+#        self.check_error(error)
+#           
