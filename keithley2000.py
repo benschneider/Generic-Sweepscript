182c2c68480ddc705e09ec7bb0553fffe0bf4871
Ben <benh.schneider@gmail.com>
2016-04-12 15:34:27 +0200

    ASCII to Binary communication +10% speed

--- keithley2000.py
+++ keithley2000.py
@@ -7,6 +7,7 @@ Keithley 2000 driver
 
 
 import visa
+from struct import unpack
 from time import time, sleep
 from parsers import savemtx, make_header, ask_overwrite
 import numpy as np
@@ -20,7 +21,7 @@ class instrument():
     a ask
     '''
 
-    def __init__(self, adress, name= 'Keithley 2000 Voltmeter'):
+    def __init__(self, adress, name= 'Keithley 2000 Voltmeter', dname='V'):
         self.name = name
         self._adress = adress
         self._visainstrument = rm.open_resource(self._adress)
@@ -28,6 +29,7 @@ class instrument():
         sleep(0.4)
         self.testspeed()
         self.testspeed()
+        #self.dname = dname
 
     def w(self,write_cmd):
         self._visainstrument.write(write_cmd)
@@ -55,7 +57,9 @@ class instrument():
         self.w('TRIG:DEL:AUTO OFF')                   #set triger delay to manual
         self.w('TRIG:DEL 0')                          #TRIGger:DELay to 0 sec
         self.w('TRIGger:COUNt 1')
-
+        #self.w(':FORM ASCII')                  #32ms
+        #self.w(':FORM DREAL')                   # 9% comm speedup (29ms)
+        self.w(':FORM SREAL')                  # 10% comm speedup (27-28ms)
 
     def set_aver(self):
         self.w(':VOLT:DC:NPLC 0.1')
@@ -67,27 +71,40 @@ class instrument():
         self.testspeed()
 
        
-    def get_val(self):
+    def get_val_ascii(self):
+        ''' works if mode is set to ASCII'''
         return eval(self.a('READ?'))
     
+    def get_val_dreal(self):
+        ''' works if mode is set to DREAL'''
+        self.w('READ?')
+        a = self._visainstrument.read_raw()
+        return unpack('d', a[2:-1])[0]
+        
+    def get_val(self):
+        ''' works if mode is set to DREAL'''
+        self.w('READ?')
+        a = self._visainstrument.read_raw()
+        return unpack('f', a[2:-1])[0]
+
     def testspeed(self):
         t00 = time()
         self.get_val()
         t01 = time()
         print 'less than 32ms is good, '+str(t01-t00) +'s'
 
-#Data storage stuff
     def prepare_data_save(self, folder, filen_0, dim_1, dim_2, dim_3, colour_name):
+        '''Prepare an empty matrix which will be filled with measurement data'''
         self._folder = folder
         self._filen_1 = filen_0 + '.mtx'
         self._head_1 = make_header(dim_1, dim_2, dim_3, colour_name)
-        self._matrix3d_1 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+        self.matrix3d_1 = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
                 
     def record_data(self,vdata,kk,jj,ii=1):
-        self._matrix3d_1[kk,jj,ii] = vdata
+        self.matrix3d_1[kk,jj,ii] = vdata
         
     def save_data(self):
-        savemtx(self._folder + self._filen_1, self._matrix3d_1, header = self._head_1)
+        savemtx(self._folder + self._filen_1, self.matrix3d_1, header = self._head_1)
                 
     def ask_overwrite(self):
         ask_overwrite(self._folder+self._filen_1)
