73492a74d5f4bd44963fe676e8a906accc75bc76
Ben <benh.schneider@gmail.com>
2016-04-12 09:17:44 +0200

    slightly more readable

--- DataStorer.py
+++ DataStorer.py
@@ -35,13 +35,9 @@ class DataStoreSP():
         self._matrix3d_2[kk, jj, ii] = vdata
 
     def save_data(self):
-        savemtx(self._folder + self._filen_1,
-                self._matrix3d_1,
-                header=self._head_1)
+        savemtx(self._folder + self._filen_1, self._matrix3d_1, header=self._head_1)
         if self.UD is True:
-            savemtx(self._folder + self._filen_2,
-                    self._matrix3d_2,
-                    header=self._head_1)
+            savemtx(self._folder + self._filen_2, self._matrix3d_2, header=self._head_1)
 
 
 class DataStore4Vec():
@@ -78,18 +74,10 @@ class DataStore4Vec():
             self._matrix3d_4[kk, jj] = np.unwrap(self._phase_data)
 
     def save_data(self):
-        savemtx(self._folder + self._filen_1,
-                self._matrix3d_1,
-                header=self._head_1)
-        savemtx(self._folder + self._filen_2,
-                self._matrix3d_2,
-                header=self._head_2)
-        savemtx(self._folder + self._filen_3,
-                self._matrix3d_3,
-                header=self._head_3)
-        savemtx(self._folder + self._filen_4,
-                self._matrix3d_4,
-                header=self._head_4)
+        savemtx(self._folder + self._filen_1, self._matrix3d_1, header=self._head_1)
+        savemtx(self._folder + self._filen_2, self._matrix3d_2, header=self._head_2)
+        savemtx(self._folder + self._filen_3, self._matrix3d_3, header=self._head_3)
+        savemtx(self._folder + self._filen_4, self._matrix3d_4, header=self._head_4)
 
 
 class DataStore2Vec():
@@ -126,17 +114,6 @@ class DataStore11Vec():
 
     def __init__(self, folder, filen_0, dim_1, dim_2, dim_3, label = '_'):
         self._folder = folder
-        # 0: <I1I1>
-        # 1: <Q1Q1>
-        # 2: <I2I2>
-        # 3: <Q2Q2>
-        # 4: <I1Q1>
-        # 5: <I2Q2>
-        # 6: <I1I2>
-        # 7: <Q1Q2>
-        # 8: <I1Q2>
-        # 9: <Q1I2>
-        # 10: <Squeezing>
         self._filen_1 = filen_0 + label + '_cI1I1' + '.mtx'
         self._filen_2 = filen_0 + label + '_cQ1Q1' + '.mtx'
         self._filen_3 = filen_0 + label + '_cI2I2' + '.mtx'
@@ -188,36 +165,14 @@ class DataStore11Vec():
         self._matrix3d_10[:, jj, ii] = data[10,:]
 
     def save_data(self):
-        savemtx(self._folder + self._filen_1,
-                self._matrix3d_0,
-                header=self._head_0)
-        savemtx(self._folder + self._filen_2,
-                self._matrix3d_1,
-                header=self._head_1)
-        savemtx(self._folder + self._filen_3,
-                self._matrix3d_2,
-                header=self._head_2)
-        savemtx(self._folder + self._filen_4,
-                self._matrix3d_3,
-                header=self._head_3)
-        savemtx(self._folder + self._filen_5,
-                self._matrix3d_4,
-                header=self._head_4)
-        savemtx(self._folder + self._filen_6,
-                self._matrix3d_5,
-                header=self._head_5)
-        savemtx(self._folder + self._filen_7,
-                self._matrix3d_6,
-                header=self._head_6)
-        savemtx(self._folder + self._filen_8,
-                self._matrix3d_7,
-                header=self._head_7)
-        savemtx(self._folder + self._filen_9,
-                self._matrix3d_8,
-                header=self._head_8)
-        savemtx(self._folder + self._filen_10,
-                self._matrix3d_9,
-                header=self._head_9)
-        savemtx(self._folder + self._filen_11,
-                self._matrix3d_10,
-                header=self._head_10)
+        savemtx(self._folder + self._filen_1, self._matrix3d_0, header=self._head_0)
+        savemtx(self._folder + self._filen_2, self._matrix3d_1, header=self._head_1)
+        savemtx(self._folder + self._filen_3, self._matrix3d_2, header=self._head_2)
+        savemtx(self._folder + self._filen_4, self._matrix3d_3, header=self._head_3)
+        savemtx(self._folder + self._filen_5, self._matrix3d_4, header=self._head_4)
+        savemtx(self._folder + self._filen_6, self._matrix3d_5, header=self._head_5)
+        savemtx(self._folder + self._filen_7, self._matrix3d_6, header=self._head_6)
+        savemtx(self._folder + self._filen_8, self._matrix3d_7, header=self._head_7)
+        savemtx(self._folder + self._filen_9, self._matrix3d_8, header=self._head_8)
+        savemtx(self._folder + self._filen_10, self._matrix3d_9, header=self._head_9)
+        savemtx(self._folder + self._filen_11, self._matrix3d_10, header=self._head_10)
