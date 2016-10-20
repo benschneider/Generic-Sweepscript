ab7b47d09dafea7fd5cfb4109dd9d7460eb6568e
Ben <benh.schneider@gmail.com>
2016-10-20 12:46:34 +0200

    deactivated HP filter

--- covfunc.py
+++ covfunc.py
@@ -88,10 +88,10 @@ def getCovMatrix(I1, Q1, I2, Q2, lags=20):
     Q1 = np.asarray(Q1)
     I2 = np.asarray(I2)
     Q2 = np.asarray(Q2)
-    CovMat = np.zeros([12, lags*2-1])
+    CovMat = np.zeros([14, lags*2+1])
 
-    start = len(I1*2-1)-lags
-    stop = len(I1*2-1)-1+lags
+    start = len(I1) - lags - 1  # len(I1*2-1)-lags
+    stop = len(I1) + lags  # len(I1*2-1)-1+lags
 
     # assume I1 Q1 have the same shape
     sI1 = np.array(I1.shape)
@@ -99,7 +99,7 @@ def getCovMatrix(I1, Q1, I2, Q2, lags=20):
     complex_result = (np.issubdtype(I1.dtype, np.complex) or
                       np.issubdtype(Q2.dtype, np.complex))
     shape = sI1 + sQ2 - 1
-    HPfilt = (int(sI1/(lags*4)))  # smallest features visible is lamda/4
+    # HPfilt = (int(sI1/(lags*4)))  # smallest features visible is lamda/4
 
     # Speed up FFT by padding to optimal size for FFTPACK
     fshape = [_next_regular(int(d)) for d in shape]
@@ -116,16 +116,16 @@ def getCovMatrix(I1, Q1, I2, Q2, lags=20):
             rfftQ1 = rfftn(Q1[::-1], fshape)
             rfftI2 = rfftn(I2[::-1], fshape)
             rfftQ2 = rfftn(Q2[::-1], fshape)
+            # filter frequencies outside the lags range (This is buggy atm)
+            # fftI1 = np.concatenate((np.zeros(HPfilt), fftI1[HPfilt:]))
+            # fftQ1 = np.concatenate((np.zeros(HPfilt), fftQ1[HPfilt:]))
+            # fftI2 = np.concatenate((np.zeros(HPfilt), fftI2[HPfilt:]))
+            # fftQ2 = np.concatenate((np.zeros(HPfilt), fftQ2[HPfilt:]))
             # filter frequencies outside the lags range
-            fftI1 = np.concatenate((np.zeros(HPfilt), fftI1[HPfilt:]))
-            fftQ1 = np.concatenate((np.zeros(HPfilt), fftQ1[HPfilt:]))
-            fftI2 = np.concatenate((np.zeros(HPfilt), fftI2[HPfilt:]))
-            fftQ2 = np.concatenate((np.zeros(HPfilt), fftQ2[HPfilt:]))
-            # filter frequencies outside the lags range
-            rfftI1 = np.concatenate((np.zeros(HPfilt), rfftI1[HPfilt:]))
-            rfftQ1 = np.concatenate((np.zeros(HPfilt), rfftQ1[HPfilt:]))
-            rfftI2 = np.concatenate((np.zeros(HPfilt), rfftI2[HPfilt:]))
-            rfftQ2 = np.concatenate((np.zeros(HPfilt), rfftQ2[HPfilt:]))
+            # rfftI1 = np.concatenate((np.zeros(HPfilt), rfftI1[HPfilt:]))
+            # rfftQ1 = np.concatenate((np.zeros(HPfilt), rfftQ1[HPfilt:]))
+            # rfftI2 = np.concatenate((np.zeros(HPfilt), rfftI2[HPfilt:]))
+            # rfftQ2 = np.concatenate((np.zeros(HPfilt), rfftQ2[HPfilt:]))
             # 0: <I1I1>
             CovMat[0, :] = (irfftn((fftI1*rfftI1))[fslice].copy()[start:stop] / len(fftI1))
             # 1: <Q1Q1>
@@ -148,8 +148,13 @@ def getCovMatrix(I1, Q1, I2, Q2, lags=20):
             CovMat[9, :] = (irfftn((fftQ1*rfftI2))[fslice].copy()[start:stop] / len(fftI1))
             # 10: <Squeezing> Magnitude
             CovMat[10, :] = (abs(1j*(CovMat[8, :]+CovMat[9, :]) + (CovMat[6, :] - CovMat[7, :])))
-            # 10: <Squeezing> Angle
+            # 11: <Squeezing> Angle
             CovMat[11, :] = np.angle(1j*(CovMat[8, :]+CovMat[9, :]) + (CovMat[6, :] - CovMat[7, :]))
+            # 12: <Squeezing> Magnitude For Hyb Coupler
+            CovMat[12, :] = (abs(1j*(CovMat[6, :]+CovMat[7, :]) + (CovMat[8, :] - CovMat[9, :])))
+            # 12: Generic Absolute cross_correlation Power
+            CovMat[13, :] =  abs(CovMat[6, :])+abs(CovMat[7, :]) + abs(CovMat[8, :]) + abs(CovMat[9, :])
+            CovMat = f1pN(CovMat, lags, d=1)  # correct Trigger jitter
             return CovMat
 
         finally:
@@ -168,6 +173,22 @@ def getCovMatrix(I1, Q1, I2, Q2, lags=20):
             pass  # ret = ret.real
 
     return CovMat
+
+
+def f1pN(CovMat, lags0, d=1): 
+    '''Simple Trigger correction function'''
+    tArray = abs(CovMat[6, :])+abs(CovMat[7, :]) + abs(CovMat[8, :]) + abs(CovMat[9, :])
+    squeezing_noise = np.sqrt(np.var(np.abs(tArray)))  # including the peak matters little
+    if np.max(np.abs(tArray[lags0 - d:lags0 + d + 1])) < 3.0 * squeezing_noise:
+        # logging.debug('SN ratio too low: Can not find trigger position')
+        distance = 0
+    else:
+        distance = (np.argmax(tArray[lags0 - d:lags0 + d + 1]) - d) * - 1  # fround trigger jitter distance
+
+    for i in range(6,14):
+        CovMat[i, :] = np.roll(CovMat[i, :], distance)  # correct Trigger jitter
+
+    return CovMat    
 
 
 def covConv(a, b, lags=20):
